7861c68901668b9902442f46ac3f93b94192be2f
nathansom <nathan.sa.official@gmail.com>
2022-08-27 17:30:18 -0700

    created register POST request

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,9 +5,7 @@ using API.Entities;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
